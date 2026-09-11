daafd5a2d9141095e0ac30b32186d0136e687aa6
yiselsa02 <yisel.sa02@gmail.com>
2026-09-11 16:28:35 -0500

    Configurar despliegue del frontend Django 6

--- Modelos_ml/RegresionLineal/frontend/frontend/settings.py
+++ Modelos_ml/RegresionLineal/frontend/frontend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6rjsu95bno9$d)7^()0vo=k4po-fwu4w0&dwznvvtuc%@t#q*8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
